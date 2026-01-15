eec60c4507eed210019933a933ec530624abf2d2
Kunal Titare <kunaltitare213@gmail.com>
2026-01-15 22:53:16 +0530

    config-server application

---
+++ src/main/java/org/com/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package org.com;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConfigServerApplication.class, args);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+    }
+}
